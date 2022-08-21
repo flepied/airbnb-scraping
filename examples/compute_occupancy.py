dd69ab4a3738efec66c39bbaeaa30188f22ae1ae
Frederic Lepied <flepied@gmail.com>
2022-08-21 19:14:12 +0200

    examples/compute_occupancy.py: do not bug on empty data

--- examples/compute_occupancy.py
+++ examples/compute_occupancy.py
@@ -19,7 +19,7 @@ for f in files:
     with open(f) as json_file:
         try:
             data = json.load(json_file)
-        except:
+        except Exception:
             continue
     invalid_dates = []
     for key in data:
@@ -31,8 +31,9 @@ for f in files:
     for key in data:
         if data[key]:
             blocked += 1
-    print(
-        f"{blocked/len(data)*100:#.2f}% for https://www.airbnb.com/rooms/{os.path.basename(f)[:-5]}"
-    )
+    if len(data) > 0:
+        print(
+            f"{blocked/len(data)*100:#.2f}% for https://www.airbnb.com/rooms/{os.path.basename(f)[:-5]}"
+        )
 
 # compute_occupancy.py ends here
